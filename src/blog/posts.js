487550dbcc78118a2102bc039263139c2ceee7ae
Christopher <chrismartinez.2054@gmail.com>
2024-09-26 23:55:35 -0600

    feat: set up initial routes for blog project

---
+++ src/blog/posts.js
@@ -0,0 +1,15 @@
+// src/blog/posts.js
+export const posts = [
+    {
+      title: "Mi primer post",
+      slug: "primer-post",
+      content: "Este es el contenido de mi primer post.",
+      date: "2024-09-27"
+    },
+    {
+      title: "Aprendiendo Astro",
+      slug: "aprendiendo-astro",
+      content: "Contenido sobre cómo estoy aprendiendo Astro.",
+      date: "2024-09-26"
+    }
+  ];
